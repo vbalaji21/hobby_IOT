d4b28303f67a554d196fa107daa1611ef96b501d
Vignesh Balaji <vignesh.balaji@atherenergy.com>
2017-04-27 20:22:06 +0530

    Add onload xmlhttp request

--- ha.js
+++ ha.js
@@ -4,27 +4,33 @@ function httpGet(theUrl)
 {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open( "GET", theUrl, true ); // false for synchronous request
-//  xmlHttp.withCredentials = true;
+  xmlHttp.onload = function () {
+    if (xmlHttp.readyState === xmlHttp.DONE) {
+      if (xmlHttp.status === 200) {
+        console.log(xmlHttp.response);
+        console.log(xmlHttp.responseXML);
+	handler(xmlHttp.responseText);
+      }
+    }
+  }
   xmlHttp.send();
-  return JSON.parse(xmlHttp.responseText);
 }
 
 document.write("Sending HTTP Request")
 while(1)
 {
-	var resp = httpGet("https://api.thingspeak.com/channels/239335/fields/1.json?api_key='Q3R57JDMK9XLYR74'")
-	resp.feeds.forEach(function(value) {
-	//	document.write("Response : " + value.field1)
-  if (value.field1 == 21) {
-		flag = "occupied";
-  } else {
-    flag = "free";
-  }
+	httpGet("https://api.thingspeak.com/channels/239335/fields/1.json?api_key='Q3R57JDMK9XLYR74'")
+}
 
-   document.querySelector("#sensor").innerHTML = "Room 1 status : " + flag
-  // document.write("Room 2 status : " + value.field1)
- //  document.write("Room 3 status : " + value.field1)
+function handler(resp) {
+  resp.feeds.forEach(function(value) {
+	// document.write("Response : " + value.field1)
+	if (value.field1 == 21) {
+	  flag = "occupied";
+	} else {
+	  flag = "free";
+	}
 
-	})
-
+  	document.querySelector("#sensor").innerHTML = "Room 1 status : " + flag
+  })
 }
