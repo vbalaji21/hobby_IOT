3cbf07218c92a2b1b60313d039b3fc3b7c1555ff
Vignesh Balaji <vignesh.balaji@atherenergy.com>
2017-06-04 14:07:18 +0530

    writting in new channel on thingspeak

--- ha.js
+++ ha.js
@@ -16,18 +16,18 @@ function httpGet(theUrl)
   xmlHttp.send();
 }
 
-document.write("Sending HTTP Request")
+//document.write("Sending HTTP Request")
 setInterval(function(){
-	httpGet("https://api.thingspeak.com/channels/239335/fields/1.json?api_key='Q3R57JDMK9XLYR74'")
+	httpGet("https://api.thingspeak.com/channels/275846/fields/1.json?api_key='DMM55Y8IBQRWNVIK'")
 }, 1000)
 
 function handler(resp) {
   resp.feeds.forEach(function(value) {
 	// document.write("Response : " + value.field1)
-	if (value.field1 == 21) {
-	  flag = "occupied";
-	} else {
+	if (value.field1 == 1) {
 	  flag = "free";
+	} else {
+	  flag = "occupied";
 	}
 
   	document.querySelector("#sensor").innerHTML = "Room 1 status : " + flag
